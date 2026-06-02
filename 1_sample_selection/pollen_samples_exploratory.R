1805a737cd08c5e61402901b895675f71d004efc
jmelanson98 <jenna.melanson@ubc.ca>
2026-06-01 23:11:43 -0700

    clean reads again with new classifications

--- 1_sample_selection/pollen_samples_exploratory.R
+++ 1_sample_selection/pollen_samples_exploratory.R
@@ -6,68 +6,54 @@ source("src/misc.R")
 load.packages()
 
 #load survey data
-specimenData = as.data.frame(read.csv("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/2022specimendata.csv", sep = ",", header = T))
-specimenData2023 = as.data.frame(read.csv("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/2023specimendata.csv", sep = ",", header = T, fill = TRUE))
-samplePoints = as.data.frame(read.table("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/allsamplepoints.csv", sep = ","))
-colnames(samplePoints) = c("sample_pt", "gps","landowner","subsite")
-sampleEffort2022 = as.data.frame(read.csv("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/2022sampledata.csv", sep = ","))
-sampleEffort2023 = as.data.frame(read.csv("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/2023sampledata.csv", sep = ","))
-vegData2022 = as.data.frame(read.csv("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/2022vegetationdata.csv", sep = ","))
+specs2022 = read.csv("3_data/cleandata/fielddata/2022specimendata.csv")
+specs2023 = read.csv("3_data/cleandata/fielddata/2023specimendata.csv")
+sample2022 = read.csv("3_data/cleandata/fielddata/2022sampledata.csv")
+sample2023 = read.csv("3_data/cleandata/fielddata/2023sampledata.csv")
+vegData2022 = read.csv("3_data/cleandata/fielddata/2022vegetationdata.csv")
 vegData2022[is.na(vegData2022)] <- 0
-vegData2023 = as.data.frame(read.csv("/Users/jenna1/Documents/UBC/Bombus Project/Raw Data/2023vegetationdata.csv", sep = ","))
+vegData2023 = read.csv("3_data/cleandata/fielddata/2023vegetationdata.csv")
 vegData2023[is.na(vegData2023)] <- 0
 
 
 #clean up: remove males / bees without final_id
-specimenData %>% filter(final_id != "") -> allbees
-allworkers = allbees[str_detect(allbees$notes, "released|male|lost", negate = T),]
+allworkers2022 = specs2022 %>% 
+  filter(final_id != "")%>%
+  filter(str_detect(notes, "queen|male", negate = T))
 
-specimenData2023 %>% filter(final_id != "") -> allbees2023
-allworkers2023 = allbees2023[str_detect(allbees2023$notes, "released|male|lost", negate = T),]
+allworkers2023 = specs2023 %>% 
+  filter(final_id != "")%>%
+  filter(str_detect(notes, "queen|male", negate = T))
 
-#add subsite + date info to specimen dataframes
-#wrangle gps coordinates into shape
-samplePoints$lat = rapply(strsplit(gsub("\\(|\\).*", "", samplePoints$gps), split = " "), function(x) tail(x, 1))
-samplePoints$long = rapply(strsplit(gsub("\\(|\\).*", "", samplePoints$gps), split = " "), function(x) head(x, 3)[2])
-samplePoints = samplePoints[,c("sample_pt", "subsite", "lat", "long")]
+#add date info to specimen dataframes
+sample2022$date = paste(sample2022$day, sample2022$month, sample2022$year)
+sample2022$date = gsub(" ", "", sample2022$date, fixed = TRUE)
+sample2022$date <- as.POSIXlt(sample2022$date, format = "%d%b%y")
+sample2022$julian_date = sample2022$date$yday
 
-#join sample points to sample effort data
-colnames(sampleEffort2022)[colnames(sampleEffort2022) == "sample_point"] = "sample_pt"
-sampleEffort2022 = left_join(sampleEffort2022, samplePoints, by = "sample_pt")
-
-colnames(sampleEffort2023)[colnames(sampleEffort2023) == "sample_point"] = "sample_pt"
-sampleEffort2023 = left_join(sampleEffort2023, samplePoints, by = "sample_pt")
-
-#add julian date to sample effort data frame
-sampleEffort2022$date = paste(sampleEffort2022$day, sampleEffort2022$month, sampleEffort2022$year)
-sampleEffort2022$date = gsub(" ", "", sampleEffort2022$date, fixed = TRUE)
-sampleEffort2022$date <- as.POSIXlt(sampleEffort2022$date, format = "%d%b%y")
-sampleEffort2022$julian_date = sampleEffort2022$date$yday
-
-sampleEffort2023$date = paste(sampleEffort2023$day, sampleEffort2023$month, sampleEffort2023$year)
-sampleEffort2023$date = gsub(" ", "", sampleEffort2023$date, fixed = TRUE)
-sampleEffort2023$date <- as.POSIXlt(sampleEffort2023$date, format = "%d%b%y")
-sampleEffort2023$julian_date = sampleEffort2023$date$yday
+sample2023$date = paste(sample2023$day, sample2023$month, sample2023$year)
+sample2023$date = gsub(" ", "", sample2023$date, fixed = TRUE)
+sample2023$date <- as.POSIXlt(sample2023$date, format = "%d%b%y")
+sample2023$julian_date = sample2023$date$yday
 
 #join sample effort df to specimen df
-allData2022 = left_join(allworkers, sampleEffort2022, by = "sample_id")
-allData2023 = left_join(allworkers2023, sampleEffort2023, by = "sample_id")
+allData2022 = left_join(allworkers2022, sample2022, by = c("sample_id", "site", "round", "year"))
+allData2023 = left_join(allworkers2023, sample2023, by = c("sample_id", "site", "round", "year"))
 
 #filter down to just mixtus and impatiens with pollen
 allData2022 %>% 
-  filter(pollen == "1" | pollen == "2" | pollen == "y") %>%
+  filter(pollen == "1" | pollen == "2" | pollen == "y" | pollen == "yy") %>%
   filter(final_id == "B. mixtus" | final_id == "B. impatiens") -> pollen2022
 allData2023 %>% 
-  filter(pollen == "1" | pollen == "2" | pollen == "y") %>%
+  filter(pollen == "1" | pollen == "2" | pollen == "y" | pollen == "yy") %>%
   filter(final_id == "B. mixtus" | final_id == "B. impatiens") -> pollen2023
 
 #create summary df by subsite, month for each year
 pollen2022 %>%
-  filter(subsite != "") %>%
-  group_by(final_id, subsite, month) %>%
+  group_by(final_id, site, month) %>%
   summarize(n = n()) %>%
   ungroup() -> summary2022
-summary2022 %>% complete(final_id, subsite, month, fill = list(total = 0)) -> summarycomplete2022
+summary2022 %>% complete(final_id, site, month, fill = list(total = 0)) -> summarycomplete2022
 summarycomplete2022$n <- ifelse(is.na(summarycomplete2022$n), 0, 
                                 ifelse(summarycomplete2022$n > 10, 10,
                                        ifelse(summarycomplete2022$n < 3, 0, summarycomplete2022$n)))
@@ -75,11 +61,11 @@ summarycomplete2022$n <- ifelse(is.na(summarycomplete2022$n), 0,
 summarycomplete2022$year = 2022
 
 pollen2023 %>%
-  filter(subsite != "") %>%
-  group_by(final_id, subsite, month) %>%
+  filter(site != "") %>%
+  group_by(final_id, site, month) %>%
   summarize(n = n()) %>%
   ungroup() -> summary2023
-summary2023 %>% complete(final_id, subsite, month, fill = list(total = 0)) -> summarycomplete2023
+summary2023 %>% complete(final_id, site, month, fill = list(total = 0)) -> summarycomplete2023
 summarycomplete2023$n <- ifelse(is.na(summarycomplete2023$n), 0, 
                                 ifelse(summarycomplete2023$n > 10, 10,
                                        ifelse(summarycomplete2023$n < 3, 0, summarycomplete2023$n)))
@@ -87,7 +73,7 @@ summarycomplete2023$year = 2023
 
 fullsummary = rbind(summarycomplete2022, summarycomplete2023)
 fullsummary$monthyear = paste(fullsummary$month, fullsummary$year, sep="")
-fullsummary = filter(fullsummary, !(round.x %in% c(11, 12)))
+fullsummary = fullsummary %>% filter(round != 11 & round != 12)
 fullsummary$monthyear <- factor(fullsummary$monthyear, levels = c("May2022", "June2022", "July2022", "August2022",
                                                                   "May2023", "June2023", "July2023", "August2023"))
 
@@ -95,16 +81,16 @@ fullmix = fullsummary[fullsummary$final_id == "B. mixtus",]
 fullimp = fullsummary[fullsummary$final_id == "B. impatiens",]
 
 #make heatmaps of which combos we have enough pollen for
-ggplot(fullmix, aes(x = monthyear, y = subsite, fill = n)) +
+ggplot(fullmix, aes(x = monthyear, y = site, fill = n)) +
   geom_tile() +
   scale_fill_gradient(low = "white", high = "orange") + # Color scale
-  geom_vline(xintercept = 10.5, color = "black", linewidth = 1) +
+  geom_vline(xintercept = 4.5, color = "black", linewidth = 1) +
   labs(title = "B. mixtus availability by subsite", x = "Timepoints", y = "subsite", fill = "Enough pollen?") +
   theme_minimal() +
   theme(legend.position = "right",
         axis.text.y = element_text(size = 8))
 
-ggplot(fullimp, aes(x = monthyear, y = subsite, fill = n)) +
+ggplot(fullimp, aes(x = monthyear, y = site, fill = n)) +
   geom_tile() +
   scale_fill_gradient(low = "white", high = "red") + # Color scale
   labs(title = "B. impatiens availability by subsite", x = "Timepoints", y = "subsite", fill = "Enough pollen?") +
@@ -113,178 +99,57 @@ ggplot(fullimp, aes(x = monthyear, y = subsite, fill = n)) +
   theme(legend.position = "none",
         axis.text.y = element_text(size = 8))
 
-#potential sample selection
-pollen2022 %>% 
-  filter(subsite != "") %>%
-  group_by(subsite, month, final_id) %>%
-  filter(n() >= 5) %>%
-  slice_sample(n=5) -> subsiteselected2022
-pollen2023 %>% 
-  filter(subsite != "") %>%
-  group_by(subsite, month, final_id) %>%
-  filter(n() >= 5) %>%
-  slice_sample(n=5) -> subsiteselected2023
-
-pollen2022 %>%
-  group_by(site.x, month, final_id) %>%
-  filter(n() >= 5) %>%
-  slice_sample(n=5) -> siteselected2022
-pollen2023 %>%
-  group_by(site.x, month, final_id) %>%
-  filter(n() >= 5) %>%
-  slice_sample(n=5) -> siteselected2023
-
-# pollen2022 %>%
-#   group_by(sample_pt.x, month, final_id) %>%
-#   filter(n() >= 3) %>%
-#   slice_sample(n=5) -> transectselected2022
-# pollen2023 %>%
-#   group_by(sample_pt.x, month, final_id) %>%
-#   filter(n() >= 3) %>%
-#   slice_sample(n=5) -> transectselected2023
-
-all2022 = rbind(subsiteselected2022, siteselected2022)
-all2023 = rbind(subsiteselected2023, siteselected2023)
-
-unique2022 = all2022 %>%
-  distinct() 
-unique2023 = all2023 %>%
-  distinct()
-
-
-  group_by(subsite, month, final_id) %>%
-  summarize(n = n_distinct(sample_pt.x)) -> subsitesum23
-  
-unique2022 %>%
-  group_by(final_id, sample_pt.x) %>%
-  summarize(n=n()) -> out
-
-dim(unique2022 %>%
-  filter(final_id == "B. impatiens"))
-
-
-
-
-
-
-
-
-
-
-
-#repeat for mixtus 2023
-#clean up: remove males / bees without final_id
-specimenData2023 %>% filter(final_id != "" & barcode_id != "NA") %>%
-  filter(final_id == "B. impatiens") -> cleanedmixtus
-cleanedmixtus = cleanedmixtus[str_detect(cleanedmixtus$notes, "released|male|lost", negate = T),]
-
-#join sample points to sample effort data
-colnames(sampleEffort2023)[colnames(sampleEffort2023) == "sample_point"] = "sample_pt"
-sampleEffort2023 = left_join(sampleEffort2023, samplePoints, by = "sample_pt")
-
-#add julian date to sample effort data frame
-sampleEffort2023$date = paste(sampleEffort2023$day, sampleEffort2023$month, sampleEffort2023$year)
-sampleEffort2023$date = gsub(" ", "", sampleEffort2023$date, fixed = TRUE)
-sampleEffort2023$date <- as.POSIXlt(sampleEffort2023$date, format = "%d%b%y")
-sampleEffort2023$julian_date = sampleEffort2023$date$yday
-
-#join sample effort df to specimen df
-allData2023 = left_join(cleanedmixtus, sampleEffort2023, by = "sample_id")
-
-
-
-
-allData2023 %>%
-  filter(pollen != "n" & pollen != "0") %>%
-  group_by(month, subsite) %>%
-  summarize(n=n()) %>%
-  filter(n >= 4 & subsite != "") -> summary
-
-#4 specimens * 45 subsite x months = 180 specimens
-# 4 specimens * 50 subsite x months = 200 specimens
-# mixtus: 320 (for landscape stuff)
-# impatiens: 360 (for landscape stuff)
-
-#680 out of 760 or 1140 --> so we can use another 80 or 460, depending on # of runs
-
 
 #plot overlap of mixtus and impatiens in each year
-#clean up: remove males / bees without final_id
-specimenData %>% filter(final_id != "") %>%
-  filter(final_id == "B. impatiens" | final_id == "B. mixtus") -> cleanedspecs
-cleanedspecs = cleanedspecs[str_detect(cleanedspecs$notes, "released|male|lost", negate = T),]
-allData2022 = left_join(cleanedspecs, sampleEffort2022, by = "sample_id")
-
-specimenData2023 %>% filter(final_id != "") %>%
-  filter(final_id == "B. impatiens" | final_id == "B. mixtus") -> cleanedspecs2023
-cleanedspecs2023 = cleanedspecs2023[str_detect(cleanedspecs2023$notes, "released|male|lost", negate = T),]
-allData2023 = left_join(cleanedspecs2023, sampleEffort2023, by = "sample_id")
 
 
-#make some plots
-allData2022 %>%
-  group_by(final_id, julian_date) %>%
-  summarize(num = n()) -> phenology2022
+# Get capture rates per day
+phenology2022 = allData2022 %>%
+  filter(final_id == "B. mixtus" | final_id == "B. impatiens") %>%
+  group_by(final_id, site, julian_date) %>%
+  summarize(num = n())
+phenology2023 = allData2023 %>%
+  filter(final_id == "B. mixtus" | final_id == "B. impatiens") %>%
+  group_by(final_id, site, julian_date) %>%
+  summarize(num = n())
 
-mix2022all = allData2022[allData2022$final_id == "B. mixtus",]
-imp2022all = allData2022[allData2022$final_id == "B. impatiens",]
-mix2022 = phenology2022[phenology2022$final_id == "B. mixtus",]
-imp2022 = phenology2022[phenology2022$final_id == "B. impatiens",]
+effort2022 = sample2022 %>%
+  group_by(site, julian_date) %>%
+  summarize(num_surveys = n())
+effort2023 = sample2023 %>%
+  group_by(site, julian_date) %>%
+  summarize(num_surveys = n())
 
-allData2023 %>%
-  group_by(final_id, julian_date) %>%
-  summarize(num = n()) -> phenology2023
+phenology2022 = phenology2022 %>%
+  left_join(effort2022) %>%
+  mutate(rate = num/num_surveys) %>%
+  mutate(year = 2022)
+phenology2023 = phenology2023 %>%
+  left_join(effort2023) %>%
+  mutate(rate = num/num_surveys) %>%
+  mutate(year = 2023)
 
-mix2023all = allData2023[allData2023$final_id == "B. mixtus",]
-imp2023all = allData2023[allData2023$final_id == "B. impatiens",]
-mix2023 = phenology2023[phenology2023$final_id == "B. mixtus",]
-imp2023 = phenology2023[phenology2023$final_id == "B. impatiens",]
-
-x_limits <- range(c(81, 235))
-
-# Create the first histogram
-hist1 <- ggplot(mix2022all, aes(x = julian_date, fill = site.x)) +
-  geom_histogram(binwidth = 0.5, position = "stack", alpha = 0.7) +
-  scale_x_continuous(limits = x_limits) +
-  theme_minimal() +
-  labs(title = "Mixtus 2022")
-
-# Create the second histogram
-hist2 <- ggplot(imp2022all, aes(x = julian_date, fill = site.x)) +
-  geom_histogram(binwidth = 0.5, position = "stack", alpha = 0.7) +
-  scale_x_continuous(limits = x_limits) +
-  theme_minimal() +
-  labs(title = "Impatiens 2022")
-
-# Create the third histogram
-hist3 <- ggplot(mix2023all, aes(x = julian_date, fill = site.x)) +
-  geom_histogram(binwidth = 0.5, position = "stack", alpha = 0.7) +
-  scale_x_continuous(limits = x_limits) +
-  theme_minimal() +
-  labs(title = "Mixtus 2023")
-
-# Create the fourth histogram
-hist4 <- ggplot(imp2023all, aes(x = julian_date, fill = site.x)) +
-  geom_histogram(binwidth = 0.5, position = "stack", alpha = 0.7) +
-  scale_x_continuous(limits = x_limits) +
-  theme_minimal() +
-  labs(title = "Impatiens 2023")
-
-# Stack the plots vertically, ensuring alignment
-final_plot <- (hist1 | hist3) / (hist2 | hist4)
-print(final_plot)      
+phenology = rbind(phenology2022, phenology2023)
 
 
-allData2022 %>%
-  filter(pollen != "n" & pollen != "0") %>%
-  group_by(sample_pt.x, final_id, round.x) %>%
-  summarize(n=n()) %>%
-  filter(n>=5) -> out
+phenologyfig = ggplot(phenology, aes(x = julian_date, y = rate, fill = site), colour = "black") +
+  geom_col(position = "identity", alpha = 0.6) +
+  facet_grid(final_id~year) +
+  scale_fill_viridis_d(option = "turbo") +
+  ylab("Captures per 5 minutes") +
+  xlab ("Day of year") +
+  labs(fill ="Landscape") +
+  xlim(c(120, 235)) +
+  theme_minimal()  +
+  theme(strip.text.y = element_text(face = "italic"))
+ggsave("7_figures/appendix_figures/speciesphenology.png",
+       width = 3000, height = 1500, units = "px")
+
 
 
 
 #plot mixtus vs impatiens abundance for 2022
-specimenData2023_joined = left_join(specimenData2023, sampleEffort2023, by = "sample_id")
+specimenData2023_joined = left_join(specimenData2023, sample2023, by = "sample_id")
 id_withpollen = specimenData2023 %>% filter(final_id == "B. mixtus") %>%
   filter(pollen == "1" | pollen == "2" | pollen =="y") %>%
   group_by(sample_id) %>%
@@ -324,13 +189,13 @@ mixtus2022workers$pollPres = ifelse(mixtus2022workers$pollen == "y" |
                                       mixtus2022workers$pollen =="2", 1, 0)
 
 #standardize data in sampleEffort frame
-sampleEffort2022$julian_date = scale(sampleEffort2022$julian_date, center = TRUE, scale = TRUE)
+sample2022$julian_date = scale(sample2022$julian_date, center = TRUE, scale = TRUE)
 landscapeMetrics$prop_blueberry = scale(landscapeMetrics$prop_blueberry, center = TRUE, scale = TRUE)
 landscapeMetrics$prop_edge = scale(landscapeMetrics$prop_edge, center = TRUE, scale = TRUE)
 landscapeMetrics$landscape_shdi = scale(landscapeMetrics$landscape_shdi, center = TRUE, scale = TRUE)
 
 mixtus2022frame = mixtus2022workers %>%
-  left_join(sampleEffort2022 %>% select(-c("site", "round", "sample_pt", "year")), by = "sample_id")
+  left_join(sample2022 %>% select(-c("site", "round", "sample_pt", "year")), by = "sample_id")
 mixtus2022frame = merge(mixtus2022frame, landscapeMetrics, by = "sample_pt")
 mixtus2022frame %>% group_by(sample_pt) %>% summarize(n=n()) -> out
 
--- 4_bioinformatics/decontaminate.R
+++ 4_bioinformatics/decontaminate.R
@@ -35,34 +35,27 @@ classifications = read_qza("3_data/merged_taxonomy.qza")$data
 ### Clean ASVs with low resolution
 #################################################
 
-# Get low resolution taxa
-lowres = classifications %>% filter(Taxon == "Streptophyta")
-otu_lowres = otu[rownames(otu) %in% lowres$Feature.ID,]
-print(sum(otu_lowres)/sum(otu)) # low resolution sequences make up about 7% of reads
-# blast searching says these are genuinely not identifiable (not just PlantITS gaps)
-# potentially some chimeras that made it through dada2?
-
-# Remove low resolution ASVs
-classifications_highres = classifications %>% filter(Taxon != "Streptophyta")
-otu_highres = otu[rownames(otu) %in% classifications_highres$Feature.ID,]
-
-# Tranpose otu table and pivot longer for plotting
-otut = as.data.frame(t(otu_highres))
+# Tranpose otu table and pivot longer
+otut = as.data.frame(t(otu))
 otut$barcode_id = rownames(otut)
-otu_long = pivot_longer(otut, cols = -barcode_id, names_to = "Feature.ID", values_to = "ReadCount")
-otu_long = otu_long %>%
+
+otu_long = pivot_longer(otut, cols = -barcode_id, names_to = "Feature.ID", values_to = "ReadCount") %>%
+  filter(ReadCount > 0) %>%
   group_by(barcode_id) %>%
   mutate(ReadProp = ReadCount/sum(ReadCount))
-otu_taxa = left_join(otu_long, classifications_highres, by = "Feature.ID")
-otu_taxa = otu_taxa %>%
-  separate(Taxon, 
-           into = c("phylum", "na_col", "order", "family", "genus", "species"),
-           sep = ";",
-           fill = "right")
 
-samples = otu_taxa %>% filter(ReadProp > 0)
-samples$scientificname = paste(samples$genus, samples$species, sep = " ")
-samples = left_join(samples, specs, by = "barcode_id")
+# Split taxon column, filter stuff not classified to genus
+otu_taxa = left_join(otu_long, classifications, by = "Feature.ID") %>%
+  separate(Taxon, 
+           into = c("k", "kingdom", "phylum", "subphylum", "order", "family", "genus", "species"),
+           sep = "__",
+           fill = "right")
+otu_filtered = otu_taxa %>%
+  filter(!is.na(genus))
+sum(otu_filtered$ReadCount)/sum(otu_taxa$ReadCount) # only 1.3% of reads are classified above genus!! damn
+
+otu_filtered$scientificname = paste(otu_filtered$genus, otu_filtered$species, sep = " ")
+samples = left_join(otu_filtered, specs, by = "barcode_id")
 
 
 #################################################
@@ -81,8 +74,8 @@ ggplot(negative_controls, aes(x = barcode_id, y = ReadCount, fill = genus)) +
   labs(x = "Sample", y = "Read count", fill = NULL) +
   theme_bw() +
   theme(legend.position = "bottom",
-        #legend.key.size = unit(0.1, "cm"),
-        #legend.text = element_text(size = 6),
+        legend.key.size = unit(0.1, "cm"),
+        legend.text = element_text(size = 4),
         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
         panel.grid = element_blank(),
         strip.background = element_rect(fill = "white"),
@@ -93,12 +86,13 @@ ggplot(negative_controls, aes(x = barcode_id, y = ReadCount, fill = genus)) +
 #################################################
 ### Remove contaminant ASVs from samples
 #################################################
-otut = as.data.frame(t(otu_highres))
+otut = as.data.frame(t(otu[rownames(otu) %in% otu_filtered$Feature.ID,]))
 
-# remove reads with constitute < 0.5% of the sample (following https://besjournals.onlinelibrary.wiley.com/doi/10.1111/2041-210X.13780)
+# remove reads which constitute < 0.5% of the sample (following https://besjournals.onlinelibrary.wiley.com/doi/10.1111/2041-210X.13780)
 rel_abund = otut / rowSums(otut)
 otu_sper = otut
 otu_sper[rel_abund < 0.005] = 0
+sum(otu_sper)/sum(otut) # lose 1.8% of reads
   
 # remove reads which are less than max contamination (following https://besjournals.onlinelibrary.wiley.com/doi/10.1111/2041-210X.13780)
 max_contam = negative_controls %>%
@@ -109,30 +103,31 @@ otu_decon = otu_sper
 for (ASV in max_contam$Feature.ID){
   otu_decon[ASV][otu_decon[ASV] < max_contam$maxReadCount[max_contam$Feature.ID == ASV]] = 0
 }
-sum(otu_decon)
+sum(otu_decon)/sum(otu_sper) # lose another 12% !!
 
 
 # first remove samples with < 1000 high quality filtered reads
-otu_decon = otu_decon[rowSums(otu_decon) > 1000,]
+otu_decon = otu_decon[rowSums(otu_decon) > 1000,colSums(otu_decon) > 0] # lose 36 samples, 687 ASVs (mostly to the 0.5% filtering step)
+sum(otu_decon)/sum(otut) # total of 14% loss with filtering, mostly from contamination
 
 write.csv(otu_decon, "3_data/cleandata/metabarcoding/cleaned_asvtable.csv")
-write.csv(classifications_highres, "3_data/cleandata/metabarcoding/qiime_classifications.csv")
+write.csv(classifications[classifications$Feature.ID %in% colnames(otu_decon)], "3_data/cleandata/metabarcoding/qiime_classifications.csv")
+
+
 #################################################
 ### Visualize samples
 #################################################
 
 otu_decon$barcode_id = rownames(otu_decon)
-otu_long = pivot_longer(otu_decon, cols = -barcode_id, names_to = "Feature.ID", values_to = "ReadCount")
-otu_long = otu_long %>%
+otu_long = pivot_longer(otu_decon, cols = -barcode_id, names_to = "Feature.ID", values_to = "ReadCount") %>%
+  filter(ReadCount > 0) %>%
   group_by(barcode_id) %>%
   mutate(ReadProp = ReadCount/sum(ReadCount))
-otu_final = left_join(otu_long, classifications_highres, by = "Feature.ID")
-otu_final = otu_final %>%
+otu_final = left_join(otu_long, classifications, by = "Feature.ID") %>%
   separate(Taxon, 
-           into = c("phylum", "na_col", "order", "family", "genus", "species"),
-           sep = ";",
-           fill = "right") %>%
-  filter(ReadProp > 0)
+           into = c("k", "kingdom", "phylum", "subphylum", "order", "family", "genus", "species"),
+           sep = "__",
+           fill = "right")
 otu_final$scientificname = paste(otu_final$genus, otu_final$species, sep = " ")
 otu_final = left_join(otu_final, specs, by = "barcode_id")
 
@@ -140,7 +135,7 @@ samples2022 = otu_final %>% filter(year == 2022) %>%
   filter(final_id == "B. mixtus" | final_id == "B. impatiens") %>%
   filter(ReadCount > 0)
 
-temp2022 = ggplot(samples2022, aes(x = barcode_id, y = ReadProp, fill = genus)) +
+temp2022 = ggplot(samples2022, aes(x = barcode_id, y = ReadProp, fill = scientificname)) +
   geom_col(width = 0.9, colour = "white", linewidth = 0.1) +
   #scale_fill_manual(values = shuffled_colors) +
   scale_fill_viridis_d(option = "turbo") +
@@ -148,15 +143,15 @@ temp2022 = ggplot(samples2022, aes(x = barcode_id, y = ReadProp, fill = genus)) 
   facet_grid(final_id ~ site, scales = "free", space = "free_x") +
   labs(x = "Sample", y = "Relative abundance", fill = NULL) +
   theme_bw() +
-  theme(legend.position = "bottom",
+  theme(legend.position = "none",
         legend.key.size = unit(0.3, "cm"),
         legend.text = element_text(size = 6),
         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 4),
         panel.grid = element_blank(),
         strip.background = element_rect(fill = "white"),
         strip.text = element_text(face = "bold")) +
-  guides(fill = guide_legend(ncol = 20))
-ggsave("6_figures/2022_pollen_consumption.png", temp2022, width = 14, height = 8, dpi = 300)
+  guides(fill = guide_legend(ncol = 9))
+ggsave("7_figures/2022_pollen_consumption.png", temp2022, width = 14, height = 8, dpi = 300)
 
 
 
@@ -192,7 +187,7 @@ blueberrymixtus = ggplot(samplesmixtus, aes(x = barcode_id, y = ReadProp, fill =
         strip.background = element_rect(fill = "white"),
         strip.text = element_text(face = "bold")) +
   guides(fill = guide_legend(ncol = 20))
-ggsave("6_figures/mixtus_blueberry_consumption.png", blueberrymixtus, width = 18, height = 8, dpi = 400)
+ggsave("7_figures/mixtus_blueberry_consumption.png", blueberrymixtus, width = 18, height = 8, dpi = 400)
 
 tempmixtus = ggplot(samplesmixtus, aes(x = barcode_id, y = ReadProp, fill = scientificname)) +
   geom_col(width = 0.9, colour = "white", linewidth = 0.1) +
@@ -208,5 +203,5 @@ tempmixtus = ggplot(samplesmixtus, aes(x = barcode_id, y = ReadProp, fill = scie
         panel.grid = element_blank(),
         strip.background = element_rect(fill = "white"),
         strip.text = element_text(face = "bold")) +
-  guides(fill = guide_legend(ncol = 20))
-ggsave("6_figures/mixtus_temp_consumption.png", tempmixtus, width = 18, height = 8, dpi = 400)
+  guides(fill = guide_legend(ncol = 10))
+ggsave("7_figures/mixtus_temp_consumption.png", tempmixtus, width = 18, height = 12, dpi = 400)
